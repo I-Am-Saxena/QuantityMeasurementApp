d1d9b250bbfa6ef4696fb17e390461b132752c5e
I-Am-Saxena <amolsaxena.personal@gmail.com>
2026-04-16 15:34:49 +0530

    UC4: Added unit conversion feature

--- src/Quantity.java
+++ src/Quantity.java
@@ -10,22 +10,31 @@ public class Quantity {
         this.unit = unit;
     }
 
+    public double getValue() {
+        return value;
+    }
+
+    public LengthUnit getUnit() {
+        return unit;
+    }
+
+    // Convert to target unit
+    public Quantity convertTo(LengthUnit targetUnit) {
+        double valueInFeet = unit.toFeet(value);
+        double convertedValue = valueInFeet / targetUnit.toFeet(1);
+        return new Quantity(convertedValue, targetUnit);
+    }
+
     private double toBase() {
         return unit.toFeet(value);
     }
 
     @Override
     public boolean equals(Object obj) {
-
-        // Same reference
         if (this == obj) return true;
-
-        // Null + type check
         if (obj == null || getClass() != obj.getClass()) return false;
 
         Quantity other = (Quantity) obj;
-
-        // Compare after converting to same base (feet)
         return Double.compare(this.toBase(), other.toBase()) == 0;
     }
 }
